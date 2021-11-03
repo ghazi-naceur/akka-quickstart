8f49b96a742414483830045058be455d1b4fc30e
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-11-03 18:44:26 +0100

    Explore receiveMessage() and receive() methods

--- src/main/scala/gn/akka/quickstart/Main.scala
+++ src/main/scala/gn/akka/quickstart/Main.scala
@@ -13,6 +13,15 @@ object OrderProcessor {
   }
 }
 
+object OrderSecondProcessor {
+  final case class Order(id: Int, product: String, number: Int) // the message to be sent
+  // Akka actor needs to have an apply method that returns a Behavior of something/message
+  def apply(): Behavior[Order] = Behaviors.receive { (context, message) =>
+    context.log.info("This is the received message '{}'.", message.toString)
+    Behaviors.same // to advise the system to reuse the previous behavior. This is provided in order to avoid the allocation overhead
+  }
+}
+
 object Main extends App {
   // Actors communicate via messages in asynchronous way
   // bootstrapping the ActorSystem => entrypoint for our orders
@@ -24,5 +33,11 @@ object Main extends App {
   orderProcessor ! Order(3, "sofa", 1)
   orderProcessor ! Order(4, "bed", 2)
 
-
+  val orderSecondProcessor: ActorSystem[OrderProcessor.Order] = ActorSystem(OrderProcessor(), "actors")
+  // sending messages with ! which is an asynchronous non-blocking call == fire and forget call
+  orderSecondProcessor ! Order(5, "chair", 3)
+  orderSecondProcessor ! Order(6, "table", 2)
+  orderSecondProcessor ! Order(7, "desk", 1)
+  orderSecondProcessor ! Order(8, "sofa", 1)
+  orderSecondProcessor ! Order(9, "bed", 2)
 }
