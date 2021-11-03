3a3867a1a635ebb8066a1ad24e9be31bd6c19d0c
ghazi-naceur <ghazi.ennacer@gmail.com>
2021-11-03 19:03:51 +0100

    Explore the setup() method

--- src/main/scala/gn/akka/quickstart/Main.scala
+++ src/main/scala/gn/akka/quickstart/Main.scala
@@ -1,43 +1,61 @@
 package gn.akka.quickstart
 
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-import gn.akka.quickstart.OrderProcessor.Order
+import gn.akka.quickstart.OrderFirstProcessor.FPOrder
+import gn.akka.quickstart.OrderSecondProcessor.SPOrder
+import gn.akka.quickstart.OrderThirdProcessor.TPOrder
 
-object OrderProcessor {
-  final case class Order(id: Int, product: String, number: Int) // the message to be sent
+object OrderFirstProcessor {
+  final case class FPOrder(id: Int, product: String, number: Int) // the message to be sent
   // Akka actor needs to have an apply method that returns a Behavior of something/message
-  def apply(): Behavior[Order] = Behaviors.receiveMessage { message =>
+  def apply(): Behavior[FPOrder] = Behaviors.receiveMessage { message =>
     println(message.toString)
     Behaviors.same // to advise the system to reuse the previous behavior. This is provided in order to avoid the allocation overhead
   }
 }
 
 object OrderSecondProcessor {
-  final case class Order(id: Int, product: String, number: Int) // the message to be sent
+  final case class SPOrder(id: Int, product: String, number: Int) // the message to be sent
   // Akka actor needs to have an apply method that returns a Behavior of something/message
-  def apply(): Behavior[Order] = Behaviors.receive { (context, message) =>
+  def apply(): Behavior[SPOrder] = Behaviors.receive { (context, message) =>
     context.log.info("This is the received message '{}'.", message.toString)
     Behaviors.same // to advise the system to reuse the previous behavior. This is provided in order to avoid the allocation overhead
   }
 }
 
+object OrderThirdProcessor {
+  final case class TPOrder(id: Int, product: String, number: Int) // the message to be sent
+  // Akka actor needs to have an apply method that returns a Behavior of something/message
+  def apply(): Behavior[TPOrder] = Behaviors.setup { context =>
+    // Setting up the Actor behavior .. here I'll get the message
+    Behaviors.receiveMessage { message =>
+      // The creation of the behavior is created when the actor actually starts
+      context.log.info("This is the received message '{}'.", message.toString)
+      Behaviors.same
+    }
+  }
+}
+
+
 object Main extends App {
   // Actors communicate via messages in asynchronous way
   // bootstrapping the ActorSystem => entrypoint for our orders
-  val orderProcessor: ActorSystem[OrderProcessor.Order] = ActorSystem(OrderProcessor(), "actors")
+  val orderProcessor: ActorSystem[OrderFirstProcessor.FPOrder] = ActorSystem(OrderFirstProcessor(), "orders-1")
   // sending messages with ! which is an asynchronous non-blocking call == fire and forget call
-  orderProcessor ! Order(0, "chair", 3)
-  orderProcessor ! Order(1, "table", 2)
-  orderProcessor ! Order(2, "desk", 1)
-  orderProcessor ! Order(3, "sofa", 1)
-  orderProcessor ! Order(4, "bed", 2)
+  orderProcessor ! FPOrder(0, "chair", 3)
+  orderProcessor ! FPOrder(1, "table", 2)
+  orderProcessor ! FPOrder(2, "desk", 1)
 
-  val orderSecondProcessor: ActorSystem[OrderProcessor.Order] = ActorSystem(OrderProcessor(), "actors")
+  val orderSecondProcessor: ActorSystem[OrderSecondProcessor.SPOrder] = ActorSystem(OrderSecondProcessor(), "orders-2")
   // sending messages with ! which is an asynchronous non-blocking call == fire and forget call
-  orderSecondProcessor ! Order(5, "chair", 3)
-  orderSecondProcessor ! Order(6, "table", 2)
-  orderSecondProcessor ! Order(7, "desk", 1)
-  orderSecondProcessor ! Order(8, "sofa", 1)
-  orderSecondProcessor ! Order(9, "bed", 2)
+  orderSecondProcessor ! SPOrder(3, "chair", 3)
+  orderSecondProcessor ! SPOrder(4, "table", 2)
+  orderSecondProcessor ! SPOrder(5, "desk", 1)
+
+  val orderThirdProcessor: ActorSystem[OrderThirdProcessor.TPOrder] = ActorSystem(OrderThirdProcessor(), "orders-3")
+  // sending messages with ! which is an asynchronous non-blocking call == fire and forget call
+  orderThirdProcessor ! TPOrder(6, "chair", 3)
+  orderThirdProcessor ! TPOrder(7, "table", 2)
+  orderThirdProcessor ! TPOrder(8, "desk", 1)
 }
